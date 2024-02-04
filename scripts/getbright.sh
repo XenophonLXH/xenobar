f55489eaa9ebfd9a080bc9d6eb6c452e6bf5c016
XenophonLXH <lourenzo321@gmail.com>
2024-02-04 21:57:11 +0200

    [IMP] bar: Started building first bar

--- scripts/getbright.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-typeset -i b=$(cat /sys/class/backlight/intel_backlight/brightness)
-
-echo $(( b / 4 ))
--- scripts/getvol.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pactl get-sink-volume $(pactl get-default-sink) | awk -F '[^0-9]+' '/left:/{print $3}'
