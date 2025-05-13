35c4c2239acbf30522aea7cb7fd2aad339d3c79d
XenophonLXH <lourenzo321@gmail.com>
2025-05-13 21:47:21 +0200

    [IMP] scripts,widgets: Finalized office vpn widget

--- scripts/vpn-check.sh
+++ scripts/vpn-check.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+if [ -z "$1" ]; then
 
-VPN_STATUS=$(nmcli connection show --active | grep $OFFICE_VPN_NAME)
-
-if [ -z "$VPN_STATUS" ]; then
-    # VPN is not connected
-    echo "0"
+    /usr/bin/notify-send "Please pass a VPN name as a parameter"
     exit 1
-else
-    # VPN is connected
-    echo "1"
-    exit 0
 fi
+
+VPN_EXISTS=$(/usr/bin/nmcli con show | grep $1)
+
+if [ -z "$VPN_EXISTS" ]; then
+    /usr/bin/notify-send "VPN '$1' does not exist, create it first"
+    exit 1
+fi
+
+
+while true; do
+
+    VPN_STATUS=$(/usr/bin/nmcli connection show --active | grep $1)
+
+    if [ -z "$VPN_STATUS" ]; then
+        # VPN is not connected
+        echo "0"
+    else
+        # VPN is connected
+        echo "1"
+    fi
+    sleep 1
+done
--- scripts/vpn-toggle.sh
+++ scripts/vpn-toggle.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+if [ -z "$1" ]; then
+    /usr/bin/notify-send "Please pass a VPN name as a parameter"
+    exit 1
+fi
 
-VPN_STATUS=$(nmcli connection show --active | grep $OFFICE_VPN_NAME)
+VPN_EXISTS=$(/usr/bin/nmcli con show | grep $1)
+
+if [ -z "$VPN_EXISTS" ]; then
+    /usr/bin/notify-send "VPN '$1' does not exist, create it first"
+    exit 1
+fi
+
+VPN_STATUS=$(/usr/bin/nmcli connection show --active | grep $1)
 
 if [ -z "$VPN_STATUS" ]; then
     # VPN is not connected
-    nmcli con up id $OFFICE_VPN_NAME
-    notify-send "$OFFICE_VPN_NAME connected"
+    /usr/bin/nmcli con up id $1
+    /usr/bin/notify-send "VPN '$1' connected"
     exit 1
 else
     # VPN is connected
-    nmcli con down id $OFFICE_VPN_NAME
-    notify-send "$OFFICE_VPN_NAME disconnected"
+    /usr/bin/nmcli con down id $1
+    /usr/bin/notify-send "VPN '$1' disconnected"
     exit 1
 fi
